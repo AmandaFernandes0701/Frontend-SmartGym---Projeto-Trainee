7c926e9200de63cfb24d560b92b76c28fa9cc558
Natalia Mota <nataliamota@cpejr.com.br>
2022-10-29 21:51:30 -0300

    Adição Imagens
    adicionei as imagens nos vetores, falta centralizar com textos e resolver um problema na responsividade.

--- smartgym/src/App.js
+++ smartgym/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Routes from "./routes";
+import "./global.css";
 
 function App() {
   return (
     <div>
-      <h1>Hello Word</h1>
       <Routes/>
     </div>
   );
--- smartgym/src/components/Frame/Frame.js
+++ smartgym/src/components/Frame/Frame.js
@@ -5,6 +5,7 @@ function Frame({frame}) {
     return ( 
         <div className="frame">
             <div className="frameTitle">{frame.title}</div>
+            <div className="frameImage">{frame.image}</div>
             <div className="frameDescription">{frame.description}</div>
         </div>
     );
