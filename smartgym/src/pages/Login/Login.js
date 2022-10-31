291429a813317c01bd73acf3939d80ab2f22bd82
Gabriel Mendonça <gabrielmendonca@cpejr.com.br>
2022-10-31 18:19:37 -0300

    Funcionalidade do login

--- smartgym/src/pages/Login/Login.js
+++ smartgym/src/pages/Login/Login.js
@@ -1,10 +1,20 @@
 import React from "react";
-//import {Link} from "react-router-dom";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import "./login.css"
 
 function Login(){
+  const [email, setEmail] = useState();
+  const [senha, setPassword] = useState();
+  const history = useHistory();
+
+      function login(){
+        alert("Bem vindo!\n" + email);
+        history.push("home");
+      }
     return (
+       
       <div className="base">
         <div className="container">
           <h1>Login</h1>
@@ -12,14 +22,16 @@ function Login(){
           <Form className="inputs">
             <Form.Group controlId="Email">
               <Form.Label>Endereço de e-mail</Form.Label>
-              <Form.Control type="email" placeholder="E-mail" />
+              <Form.Control type="email" placeholder="E-mail" onChange={(e)=>setEmail(e.target.value)}/>
+              
             </Form.Group>
 
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Senha</Form.Label>
-              <Form.Control type="password" placeholder="Senha" />
+              <Form.Control type="password" placeholder="Senha" onChange={(e)=>setPassword(e.target.value)} />
             </Form.Group>
 
+            <Button variant="primary" onClick={(login)} > Login </Button>
             {/* <div className="inputs">
             </div> */}
 
