a5c7b19e0ba78e6a97abb8a53c04cde49f45287c
Amanda Fernandes <amandaalves@cpejr.com.br>
2022-11-03 03:38:27 -0300

    70% Menu Sanduiche

--- smartgym/src/pages/Menu_Sanduiche/Menu_Sanduiche.js
+++ smartgym/src/pages/Menu_Sanduiche/Menu_Sanduiche.js
@@ -1,50 +1,82 @@
-import { Drawer, List, ListItem, ListItemText, Typography } from "@mui/material";
+import { AppBar, Drawer, IconButton, List, ListItem, ListItemText, Toolbar, Typography} from "@mui/material";
 import React, { useState } from "react";
 import { IconContext, icons } from "react-icons/lib";
 import { RiHomeHeartFill } from "react-icons/ri";
-import "./Menu_Sanduiche.css"
+import { ImProfile } from "react-icons/im";
+import { BsFillPersonPlusFill } from "react-icons/bs";
+import { GiWeightLiftingUp } from "react-icons/gi";
+import { FiLogIn, FiMenu } from "react-icons/fi";
+import "./Menu_Sanduiche.css";
 import { useHistory } from "react-router-dom";
-import Home from "../Home";
+import { ClassNames } from "@emotion/react";
 
 function Menu_Sanduiche() {
     const history = useHistory();
     const [currentPage, setcurrentPage] = useState("/home");
+    const [open, setOpen] = useState(false);
 
     function handleClick(pathName) {
         history.push(pathName);
         setcurrentPage(pathName);
     }
 
+    function handleDrawer(isOpen){
+        setOpen(isOpen);
+    }
+
     const pages = [
         {
             pathName: "/home",
             icon: <RiHomeHeartFill/>,
-            text: "Home"
+            text: "Home",
+            iconSize: "1.5em"
         },
         {
             pathName: "/perfil",
-            icon: <RiHomeHeartFill/>,
-            text: "Perfil"
+            icon: <ImProfile/>,
+            text: "Perfil",
+            iconSize: "1.5em"
         },
         {
             pathName: "/cadastro",
-            icon: <RiHomeHeartFill/>,
-            text: "Cadastro"
+            icon: <BsFillPersonPlusFill/>,
+            text: "Cadastro",
+            iconSize: "1.5em"
         },
         {
             pathName: "/exercicios",
-            icon: <RiHomeHeartFill/>,
-            text: "Exercícios"
+            icon: <GiWeightLiftingUp/>,
+            text: "Exercícios",
+            iconSize: "1.5em"
         },
         {
             pathName: "/login",
-            icon: <RiHomeHeartFill/>,
-            text: "Login"
+            icon: <FiLogIn/>,
+            text: "Login",
+            iconSize: "1.5em"
         },
     ]
 
     return (
-        <Drawer open={true}>
+        <>
+        <AppBar position="static">
+            <Toolbar className="Toolbar">
+                <div className="Smart_Gym_Titulo">
+                    <p>Smart Gym</p>
+                </div>
+                <IconButton
+                    edge ="start"
+                    color = "inherit"
+                    aria-label="menu"
+                    onClick={()=> handleDrawer(!open)}
+                >
+                    <FiMenu/>
+                </IconButton>
+            </Toolbar>
+
+        </AppBar>
+
+        <Drawer open={open} onClose ={()=> handleDrawer(false)}>
 
             <List className="list">
                 {pages.map((listItem) => {
@@ -52,7 +84,7 @@ function Menu_Sanduiche() {
                         <ListItem button
                             selected={currentPage === listItem.pathName}
                             onClick={() => { handleClick(listItem.pathName) }}>
-                            <IconContext.Provider value={{ color: "#343434", size: "1.5em" }}>
+                            <IconContext.Provider value={{ color: "#343434", size: listItem.iconSize}}>
                                 {listItem.icon}
                             </IconContext.Provider>
                             <ListItemText className="ListItemText">
@@ -63,6 +95,7 @@ function Menu_Sanduiche() {
                 })}
             </List>
         </Drawer>
+        </>
     );
 }
 export default Menu_Sanduiche;
