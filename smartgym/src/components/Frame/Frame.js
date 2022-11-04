c32886a634f751f1632257b8c3026880a4647a2f
Natalia Mota <nataliamota@cpejr.com.br>
2022-11-04 18:19:24 -0300

    Estilização parcial
    problema reposição resolvido.

--- smartgym/src/components/Frame/Frame.js
+++ smartgym/src/components/Frame/Frame.js
@@ -5,7 +5,7 @@ function Frame({frame}) {
     return ( 
         <div className="frame">
             <div className="frameImage">{frame.image}</div>
-            <div>
+            <div className="frameText">
                 <div className="frameTitle">{frame.title}</div>
                 <div className="frameDescription">{frame.description}</div>
                 <button className="frameButton">{frame.button}Adicionar</button>
