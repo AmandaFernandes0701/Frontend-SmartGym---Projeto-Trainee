e62d1150aa4674fa5c61b034ac9c1cc1e6611888
Gabriel Mendonça <gabrielmendonca@cpejr.com.br>
2022-11-05 20:04:26 -0300

    Login final

--- smartgym/src/pages/Login/Login.js
+++ smartgym/src/pages/Login/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Form, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import "./Login.css"
 
 function Login(){
@@ -16,36 +16,34 @@ function Login(){
     return (
       <div className="base">
         <div className="container-login">
-          <div class="login-title">
-           LOGIN
-          </div>
-            <Form.Group controlId="Email">
-              <div class="form-group-email-label">
-              <Form.Label>Endereço de e-mail</Form.Label>
+            <div class="login-title">
+            LOGIN
+            </div>
+              <div className="linha1text">
+              <div className="textEmail">Endereço de e-mail</div>
               </div>
-              <Form.Control type="email" class="form-controle-inputs" onChange={(e)=>setEmail(e.target.value)}/>
+
+              <div className="linha1">
+                  <input className="inputEmail" type="Email" onChange={(e)=>setEmail(e.target.value)}/>
+              </div>
               
-            </Form.Group>
-
-            <Form.Group controlId="formBasicEmail">
-              <div class="form-group-pass-label">
-              <Form.Label>Senha</Form.Label>
+              <div className="linha2text">
+                <p className="textSenha">Senha</p>
               </div>
-              <Form.Control type="password" class="form-controle-inputs" onChange={(e)=>setPassword(e.target.value)} />
-            </Form.Group>
 
-            <Form.Group>
+              <div className="linha2">
+                <input className="inputSenha" type="password" onChange={(e)=>setPassword(e.target.value)} />
+              </div>
 
-            <button class="button" type="submit" oncLICK>
+
+            <button class="button" type="submit" onClick={(login)}>
               <div class="button-label">
                 ENTRAR
               </div>
               </button>
-            </Form.Group>
-            {/* <Button variant="primary" onClick={(login)} > Entrar </Button> */}
-    
         </div>
       </div>
     );
 }
+
 export default Login;
--- smartgym/src/pages/Perfil/Perfil.js
+++ smartgym/src/pages/Perfil/Perfil.js
@@ -1,6 +1,10 @@
 import React from "react";
+import "./Perfil.css"
+
 
 function Perfil(){
-    return <h1>Perfil</h1>;
+    return (
+        <h1>Perfil</h1>
+    );
 }
 export default Perfil;
