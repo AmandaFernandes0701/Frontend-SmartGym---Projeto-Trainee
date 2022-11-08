3acc23c4b545a22e1d96d0338a6b60fd430bc62d
Gabriel Mendonça <gabrielmendonca@cpejr.com.br>
2022-11-07 22:37:27 -0300

    1%

--- smartgym/src/pages/Perfil/Perfil.js
+++ smartgym/src/pages/Perfil/Perfil.js
@@ -4,8 +4,17 @@ import "./Perfil.css";
 function Perfil(){
     return (
         <div className="body">
+        <div className="marcador-pag" >
+        <div className="marcador-pagina-anterior">
+            HOME |
+        </div>
+        <div className="perfil-do-user">
+            PERFIL DO USUÁRIO
+        </div>
+        </div>
+            
         <div className="container-dados1">
-            <image src="/images/nomedoarquivo.png" alt="NomeExercicio"/>
+            {/* <image src="/images/nomedoarquivo.png" alt="NomeExercicio"/> */}
             <h1>Perfil</h1>
         </div>
         </div>
