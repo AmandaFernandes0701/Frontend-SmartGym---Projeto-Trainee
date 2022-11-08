03d1b4b4301ad8ae61cf3b88f250c3cae4b7bb4d
Gabriel Mendonça <gabrielmendonca@cpejr.com.br>
2022-11-08 00:00:30 -0300

    30%

--- smartgym/src/pages/Perfil/Perfil.js
+++ smartgym/src/pages/Perfil/Perfil.js
@@ -14,8 +14,88 @@ function Perfil(){
         </div>
             
         <div className="container-dados1">
-            {/* <image src="/images/nomedoarquivo.png" alt="NomeExercicio"/> */}
-            <h1>Perfil</h1>
+
+            <div className="linha1text">
+
+                <p className="textNome">Nome</p>
+                <div className="textTelefone">Telefone</div>
+
+            </div>
+
+            <div className="linha1">
+                <input className="inputNome"
+                 type="text"
+                  name='nome'
+                  />
+                <input className="inputTelefone"
+                type="text"
+                name='telefone' 
+                />   
+
+            </div>
+
+            <div className="linha2text">
+
+                <p className="textCPF">CPF</p>
+                <p className="textIdade">Idade</p>
+                <p className="textSexo">Sexo</p>
+            </div>
+
+            <div className="linha2">
+                <input className="inputCPF"
+                 type="text"
+                  name='cpf'
+                  />
+                <input className="inputIdade"
+                type="text"
+                name='idade' 
+                />
+                <input className="inputSexo"
+                type="text"
+                name='sexo'
+                />
+            </div>
+
+            <div className="linha3text">
+
+                <p className="textNome">Cidade/Estado</p>
+                <p className="textTelefone">Endereço</p>
+
+            </div>
+
+            <div className="linha3">
+                <input className="inputCidade"
+                 type="text"
+                  name='cidade'
+                  />
+                <input className="inputEndereco"
+                type="text"
+                name='endereco' 
+                />   
+            </div>
+
+            <div className="linha4text">
+                <p className="textNome">E-mail</p>
+            </div>
+
+            <div className="linha4">
+                <input className="inputEmail"
+                 type="text"
+                  name='email'
+                  />
+            </div>
+
+            <div className="linha5text">
+                <p className="textNome">Senha</p>
+            </div>
+
+            <div className="linha5">
+                <input className="inputSenha"
+                 type="password"
+                  name='senha'
+                  />
+            </div>
+
         </div>
         </div>
     );
