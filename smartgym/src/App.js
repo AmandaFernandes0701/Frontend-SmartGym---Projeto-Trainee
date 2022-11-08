df83ffd71ca46cf15cffad713b1465b08e178f65
Gabriel Mendonça <gabrielmendonca@cpejr.com.br>
2022-11-07 21:54:19 -0300

    Início

--- smartgym/src/App.js
+++ smartgym/src/App.js
@@ -3,10 +3,7 @@ import Routes from "./routes";
 
 function App() {
   return (
-    <div>
-      <h1>Hello Word</h1>
-      <Routes/>
-    </div>
+      <Routes />
   );
 }
 
--- smartgym/src/pages/Perfil/Perfil.js
+++ smartgym/src/pages/Perfil/Perfil.js
@@ -1,6 +1,14 @@
 import React from "react";
+import "./Perfil.css";
 
 function Perfil(){
-    return <h1>Perfil</h1>;
+    return (
+        <div className="body">
+        <div className="container-dados1">
+            <image src="/images/nomedoarquivo.png" alt="NomeExercicio"/>
+            <h1>Perfil</h1>
+        </div>
+        </div>
+    );
 }
 export default Perfil;
