fd07f86c19b30cb49e864330a2a8cdebd735dcc7
Gabriel Mendonça <gabrielmendonca@cpejr.com.br>
2022-11-09 19:01:43 -0300

    70%

--- smartgym/src/pages/Perfil/Perfil.js
+++ smartgym/src/pages/Perfil/Perfil.js
@@ -19,83 +19,92 @@ function Perfil(){
 
                 <div className="linha1text">
 
-                    <p className="textNome">Nome</p>
-                    <div className="textTelefone">Telefone</div>
-
-                </div>
-
-                <div className="linha1">
-                    <input className="inputNome"
-                    type="text"
-                    name='nome'
+                    <div className="bloco1">
+                        <p className="textNome">Nome</p>
+                        <input className="inputNome"
+                        type="text"
+                        name='nome'
                     />
-                    <input className="inputTelefone"
-                    type="text"
-                    name='telefone' 
-                    />   
+                    </div>
 
+                    <div className="bloco2">
+                        <p className="textTelefone">Telefone</p>
+                        <input className="inputTelefone"
+                        type="text"
+                        name='telefone'
+                    />
+                    </div>
+
+        
                 </div>
+
 
                 <div className="linha2text">
+                    <div className="bloco3">
+                        <p className="textCPF">CPF</p>
+                        <input className="inputCPF"
+                        type="text"
+                        name='cpf'
+                        />
+                    </div>
+                    
+                    <div className="bloco4">
+                        <p className="textIdade">Idade</p>
+                        <input className="inputIdade"
+                        type="text"
+                        name='idade' 
+                        />
+                    </div>
 
-                    <p className="textCPF">CPF</p>
-                    <p className="textIdade">Idade</p>
-                    <p className="textSexo">Sexo</p>
-                </div>
+                    <div className="bloco5">
+                        <p className="textSexo">Sexo</p>
+                        <input className="inputSexo"
+                        type="text"
+                        name='sexo'
+                        />
+                    </div>
 
-                <div className="linha2">
-                    <input className="inputCPF"
-                    type="text"
-                    name='cpf'
-                    />
-                    <input className="inputIdade"
-                    type="text"
-                    name='idade' 
-                    />
-                    <input className="inputSexo"
-                    type="text"
-                    name='sexo'
-                    />
                 </div>
 
                 <div className="linha3text">
+                    <div className="bloco6">
+                        <p className="textCidade">Cidade/Estado</p>
+                        <input className="inputCidade"
+                        type="text"
+                        name='cidade'
+                        />
+                    
+                    </div>
+                    <div className="bloco7">
+                        <p className="textEndereco">Endereço</p>
+                        <input className="inputEndereco"
+                        type="text"
+                        name='endereco' 
+                        />
+                    </div>
 
-                    <p className="textNome">Cidade/Estado</p>
-                    <p className="textTelefone">Endereço</p>
-
-                </div>
-
-                <div className="linha3">
-                    <input className="inputCidade"
-                    type="text"
-                    name='cidade'
-                    />
-                    <input className="inputEndereco"
-                    type="text"
-                    name='endereco' 
-                    />   
                 </div>
 
                 <div className="linha4text">
-                    <p className="textNome">E-mail</p>
-                </div>
-
-                <div className="linha4">
+                    <div className="bloco8">
+                    <p className="textEmail">E-mail</p>
                     <input className="inputEmail"
                     type="text"
                     name='email'
                     />
+                    </div>
+                    
                 </div>
+
 
                 <div className="linha5text">
-                    <p className="textNome">Senha</p>
-                </div>
-
-                <div className="linha5">
-                    <input className="inputSenha"
-                    type="password"
-                    name='senha'
-                    />
+                    <div className="bloco9">
+                        <p className="textSenha">Senha</p>
+                        <input className="inputSenha"
+                        type="password"
+                        name='senha'
+                        />
+                    </div>
                 </div>
 
         </div>
