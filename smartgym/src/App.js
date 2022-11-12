19c3a62cf51094cff662b429861983d264bf9da7
Gabriel Mendonça <gabrielmendonca@cpejr.com.br>
2022-11-12 16:50:39 -0300

    .

--- smartgym/src/App.js
+++ smartgym/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Routes from "./routes";
 import "./global.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
+    <div>
+      {/* <h1></h1> */}
       <Routes />
+    </div>
   );
 }
 
