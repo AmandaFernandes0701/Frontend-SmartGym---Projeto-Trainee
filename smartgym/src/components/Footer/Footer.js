2726bb780f0a83ebeec8ab6ec5b4b513ee5d2976
Amanda Fernandes <amandaalves@cpejr.com.br>
2022-11-12 13:41:23 -0300

    Merge + Correções 2.0
    Correções do front realizadas:
    - Home 100% arrumada (responsivo + web)
    - Footer 100% arrumado (responsivo + web)
    - Navbar 100% arrumada (hover + web + responsivo)
    - Exercícios arrumado (apenas versão mobile)
    
    O que falta fazer:
    - Arrumar página do cadastro
    - Fazer login responsivo
    - Inserir página do perfil

--- smartgym/src/components/Footer/Footer.js
+++ smartgym/src/components/Footer/Footer.js
@@ -4,13 +4,14 @@ import "./Footer.css"
 function Footer(){
     return(
         <div className="body_footer">
-            <div className="Smart_Gym_footer">
+            <div className="Smart_Gym_footer_icon_e_titulo">
                 <div className="icon_smart_gym_footer">
                 <img src="/images/logo_icon.png" alt="logo_icon_imagem" className="imagem_logo_footer"/>
 
                 </div>
                 <div className="titulo_smart_gym_footer">
-                <p>SmartGym</p>
+                <p className="titulo_smart_gym_footer_SMART">Smart</p>
+                <p className="titulo_smart_gym_footer_GYM">Gym</p>
                 </div>
             </div>
 
