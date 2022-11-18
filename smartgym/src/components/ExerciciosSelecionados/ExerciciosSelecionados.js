583f5a1b3b6dfc149fb42a276e8c8b9068b2389d
Gabriel Mendonça <gabrielmendonca@cpejr.com.br>
2022-11-18 03:21:11 -0300

    Atualização das 03:20, tá funcionando, falta corrigir tamanhos das coisas

--- smartgym/src/components/ExerciciosSelecionados/ExerciciosSelecionados.js
+++ smartgym/src/components/ExerciciosSelecionados/ExerciciosSelecionados.js
@@ -5,7 +5,7 @@ function ExerciciosSelecionados({exercicio}) {
     console.log(exercicio);
     return ( 
         <div className="frame">
-            <div className="frameImage"><img style={{ width: "100%", height: "100%" }} src={exercicio.imagem} className="cerrote" alt=""/></div>
+            <div className="frameImage"><img style={{ width: "50%", height: "50%" }} src={exercicio.imagem} className="cerrote" alt=""/></div>
             <div className="frameText">
                 <div className="frameTitle">{exercicio.titulo}</div>
             </div>
--- smartgym/src/components/Frame/Frame.js
+++ smartgym/src/components/Frame/Frame.js
@@ -3,34 +3,36 @@ import "./Frame.css";
 import { getID } from "../../services/auth";
 import api from "../../services/api";
 
-function Frame({frame}) {
+function Frame({ frame }) {
+  function adicionando(e) {
+    console.log(frame);
 
-    function adicionando(e){
+    e.preventDefault();
 
-
-        e.preventDefault();
-
-        try{
-            const user_id = getID();
-        api.post('/relation', {user_id, id_exercicio: frame.id_exercicio});
-        alert("Exercício adicionado com sucesso");
-        }
-        catch (error){
-            console.log(error.message);
-        }
+    try {
+      const user_id = getID();
+      api.post("/relation", { user_id, id_exercicio: frame.id_exercicio });
+      console.log(user_id);
+      alert("Exercício adicionado com sucesso");
+      console.log(frame.id_exercicio);
+    } catch (error) {
+      console.log(error.message);
     }
+  }
 
-    return ( 
-        <div className="frame">
-            <div className="frameImage">{frame.image}</div>
-            <div className="frameText">
-                <div className="frameTitle">{frame.titulo}</div>
-                <div className="frameDescription">{frame.descricao}</div>
-                <button className="frameButton"onClick={adicionando}>Adicionar</button>
-            
-            </div>
-        </div>
-    );
+  return (
+    <div className="frame">
+      <div className="frameImage"><img style={{ width: "100%", height: "100%" }} src={frame.imagem} className="cerrote" alt=""/></div>
+        {/* CORRIGIR TAMANHO DO FRAME */}
+      <div className="frameText">
+        <div className="frameTitle">{frame.titulo}</div>
+        <div className="frameDescription">{frame.descricao}</div>
+        <button className="frameButton" onClick={adicionando}>
+          Adicionar
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Frame;
